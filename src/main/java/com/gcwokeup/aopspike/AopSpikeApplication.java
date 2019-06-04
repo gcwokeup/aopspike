5faad7fe5dacd42c1dfdd5056b0153f0df68cdb7
Gino Capio <ginocapio@gmail.com>
2019-06-04 08:12:03 -0700

    first commit

---
+++ src/main/java/com/gcwokeup/aopspike/AopSpikeApplication.java
@@ -0,0 +1,13 @@
+package com.gcwokeup.aopspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopSpikeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AopSpikeApplication.class, args);
+  }
+
+}
